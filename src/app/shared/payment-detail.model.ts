16f075ac0cdc99541916479d1eaa845e75f4b535
Alex-dev1356 <asoliven26@yahoo.com>
2026-03-07 18:46:26 +0800

    Defining the PaymentDetail Model in UI

--- src/app/shared/payment-detail.model.ts
+++ src/app/shared/payment-detail.model.ts
@@ -1,2 +1,12 @@
 export class PaymentDetail {
+
+    //In API Use PascalCasing Convention and on the UI Use CamelCasing
+    //NOTE: THE SPELLING IS IMPORTANT HERE, IT SHOULD BE SAME SPELLING AS IN API
+    //BECAUSE THE API IS CASE SENSITIVE, ONLY THE FIRST LETTER SHOULD START WITH LOWERCASE
+    public paymentDetailId: number = 0
+    public cardOwnerName: string = ''
+    public cardNumber: string = ''
+    public expirationDate: string = ''
+    public securityCode: string = ''
+
 }
