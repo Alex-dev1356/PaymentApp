e2d4ae14cee684e7034c40b080705766c7812932
Alex-dev1356 <asoliven26@yahoo.com>
2026-03-08 08:37:50 +0800

    To Show Notifcation, we use the ngx-toastr and we can go here: https://www.npmjs.com/package/ngx-toastr/v/17.0.2 and if we want to install the specific version we can use npm install ngx-toastr@17 and npm install @angular/animations@16 because the animations is a required dependency

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,6 +6,8 @@ import { PaymentDetailsComponent } from './payment-details/payment-details.compo
 import { PaymentDetailFormComponent } from './payment-detail-form/payment-detail-form.component';
 import { HttpClientModule } from '@angular/common/http';
 import { FormsModule } from '@angular/forms';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { ToastrModule } from 'ngx-toastr';
 
 @NgModule({
   declarations: [
@@ -16,7 +18,9 @@ import { FormsModule } from '@angular/forms';
   imports: [
     BrowserModule,
     HttpClientModule,
-    FormsModule
+    FormsModule,
+    BrowserAnimationsModule,
+    ToastrModule.forRoot()
   ],
   providers: [],
   bootstrap: [AppComponent]
